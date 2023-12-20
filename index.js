f48335f255903e1d6720dd669366bee4e2762a88
Bea Obiero <bea.obiero@gmail.comgit config --global user.email bea.obiero@gmail.comgit config --global user.email + + bea.obiero@gmail.com>
2023-12-20 11:38:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+//1)
+num1 = 62
+num2 = 1
+multiply = num1 * num2
+console.log(multiply);
 
-// Write your code here
+//2)
+function generateRandomNumber() {
+    return Math.ceil(Math.random() * 10);
+}
+console.log(generateRandomNumber());
+
+
+//3)
+const num3 = 25
+const num4 = 7
+function calculateMod(num3,num4) {
+    return num3 % num4; 
+}
+console.log(calculateMod(num3,num4));
+
+
+//4)
+let numbers = [15,5,10,20,1]
+function findMaxNumber (numbers) {
+    return Math.max(...numbers)
+}
+console.log(findMaxNumber(numbers));
